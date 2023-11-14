2f6c42e61a4e93b3259079e5139d3f34230d24ae
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-14 22:20:50 +0100

    Edited User class and added some methods

--- EksamensProjekt2023/Models/User.cs
+++ EksamensProjekt2023/Models/User.cs
@@ -1,15 +1,46 @@
-﻿namespace EksamensProjekt2023.Models
+﻿namespace EksamensProjekt2023.Models;
 /// <summary>
 /// Date Created: 2023-11-14 <br />
-/// Author: Muhammed Tastan
+/// Author: Muhammed Tastan <br />
+/// Date Edited: 2023-11-14 <br />
+/// Co-Author: 2023-11-14 <br />
+/// <br />
+/// 
+/// A simple user model.
 /// </summary>
+public class User
 {
-    public class User
+    private string username;
+    private string password;
+    private string firstName;
+    private string lastName;
+    private string email;
+    private int phoneNumber;
+    private bool isAdmin;
+    private bool isLeader;
+    private bool isApproved;
+
+    public User(string username, string password, string firstName, string lastName, string email, int phoneNumber)
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? UserName { get; set; }
-        public string? Email { get; set; }
-        public int PhoneNumber { get; set; }
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        isAdmin = false;
+        isLeader = false;
+        isApproved = false;
     }
+
+    public string Username => username;
+    public string Password => password;
+    public string FirstName => firstName;
+    public string LastName => lastName;
+    public string Email => email;
+    public int PhoneNumber => phoneNumber;
+
+    public void SetAdmin() => isAdmin = true;
+    public void SetLeader() => isLeader = true;
+    public void SetApproved() => isApproved = true;
 }
