23d34d0318a7318bfab3bbba3837a8c0aeaf07e4
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-14 11:39:11 +0100

    Implemented Message class

---
+++ EksamensProjekt2023/Models/Message.cs
@@ -0,0 +1,51 @@
+﻿namespace EksamensProjekt2023.Models;
+
+/// <summary>
+/// Date Created: 2023-11-14 <br />
+/// Author: Gabriel H. Kierkegaard
+/// </summary>
+public class Message
+{
+    private DateTime date;
+    private string? sender;
+    private string? reciever;
+    private int messageSize;
+    private bool isRead;
+
+    public Message(string sender, string reciever)
+    {
+        date = DateTime.Now;
+        this.sender = sender;
+        this.reciever = reciever;
+        messageSize = 255;
+        isRead = false;
+    }
+
+    public DateTime Date
+    {
+        get { return date; }
+    }
+
+    public string? Sender
+    {
+        get { return sender; }
+        set { sender = value; }
+    }
+
+    public string? Reciever
+    {
+        get { return reciever; }
+        set { reciever = value; }
+    }
+
+    public int MessageSize
+    {
+        get { return messageSize; }
+    }
+
+    public bool IsRead
+    {
+        get { return isRead; }
+        set { isRead = value; }
+    }
+}
