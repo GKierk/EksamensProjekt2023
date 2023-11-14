a805ecd843dd9b8e9485434b2d52ef6b3fab9294
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-14 11:40:06 +0100

    Implemented IMessage Interface

---
+++ EksamensProjekt2023/Interfaces/IMessage.cs
@@ -0,0 +1,12 @@
+﻿using EksamensProjekt2023.Models;
+
+namespace EksamensProjekt2023.Interfaces;
+/// <summary>
+/// Date Created: 2023-11-14 <br />
+/// Author: Gabriel H. Kierkegaard
+/// </summary>
+public interface IMessage
+{
+    public void SendMessage(Message message) { }
+    public void RecievedMessage(Message message) { }
+}
