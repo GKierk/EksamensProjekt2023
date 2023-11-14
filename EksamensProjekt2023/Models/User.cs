69769abf9ecfd19f9815046b371f5ddf9b9fcc96
Muhammed Tastan <muha082195@gmail.com>
2023-11-14 21:45:17 +0100

    Added User class

---
+++ EksamensProjekt2023/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace EksamensProjekt2023.Models
+/// <summary>
+/// Date Created: 2023-11-14 <br />
+/// Author: Muhammed Tastan
+/// </summary>
+{
+    public class User
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
