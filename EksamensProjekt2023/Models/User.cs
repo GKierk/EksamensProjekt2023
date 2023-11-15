049a1a274e854539e11134cece93231951d95f6f
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-15 11:52:44 +0100

    Changed Password to UserPassword since Password is a reserved SQL keyword

--- EksamensProjekt2023/Models/User.cs
+++ EksamensProjekt2023/Models/User.cs
@@ -12,7 +12,7 @@ public class User
 {
     public int UserID { get; set; }
     public string? Username { get; set; }
-    public string? Password { get; set; }
+    public string? UserPassword { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
