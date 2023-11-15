bead9c58caf334377ad6e4804672616dd5886e59
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-15 11:48:15 +0100

    Implemented IUserService

---
+++ EksamensProjekt2023/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using EksamensProjekt2023.Models;
+
+namespace EksamensProjekt2023.Services.Interfaces;
+/// <summary>
+/// Date Created: 2023-11-15 <br />
+/// Author: Gabriel H. Kierkegaard
+/// </summary>
+public interface IUserService
+{
+    public IEnumerable<User> GetUsers();
+}
