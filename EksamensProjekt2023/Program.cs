a156becf7396652eaa8cda1d6ee1550b10848ff7
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-15 09:18:23 +0100

    Added database service

--- EksamensProjekt2023/Program.cs
+++ EksamensProjekt2023/Program.cs
@@ -1,7 +1,11 @@
+using EksamensProjekt2023.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<TastanDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
