2c90efdf53285d0c5f44e24aea37299ca0bd3882
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-15 11:48:55 +0100

    Implemented EFUserService

---
+++ EksamensProjekt2023/Services/EFService/EFUserService.cs
@@ -0,0 +1,23 @@
+﻿using EksamensProjekt2023.Models;
+using EksamensProjekt2023.Services.Interfaces;
+
+namespace EksamensProjekt2023.Services.EFService;
+/// <summary>
+/// Date Created: 2023-11-15
+/// Author: Gabriel H. Kierkegaard
+/// </summary>
+public class EFUserService : IUserService
+{
+    private List<User>? users;
+    private TastanDBContext dBContext;
+
+    public EFUserService(TastanDBContext context)
+    {
+        dBContext = context;
+    }
+
+    public IEnumerable<User> GetUsers()
+    {
+        return dBContext.Users.ToList();
+    }
+}
