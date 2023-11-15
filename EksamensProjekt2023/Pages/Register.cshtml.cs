9b48b263243c0446e5dff9f2a3965794021b7c2f
Muhammed Tastan <muha082195@gmail.com>
2023-11-15 09:24:53 +0100

    Tried different codes to make the class work but reverted it

--- EksamensProjekt2023/Pages/Register.cshtml.cs
+++ EksamensProjekt2023/Pages/Register.cshtml.cs
@@ -6,12 +6,15 @@ namespace EksamensProjekt2023.Pages
 {
     public class RegisterModel : PageModel
     {
-        public User UserProfile { get; set; }
+        [BindProperty]
+        public User User { get; set; }
         public void OnGet()
         {
-            
-            
-           
+
+        }
+        public IActionResult OnPost() 
+        {
+            return RedirectToPage("/Index");
         }
     }
 }
