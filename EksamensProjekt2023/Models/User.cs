9aff4d4d39260e4922d724811245993d962cd57b
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-20 14:56:47 +0100

    Made User inherit from IdentityUser

--- EksamensProjekt2023/Models/User.cs
+++ EksamensProjekt2023/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
 
 namespace EksamensProjekt2023.Models;
 /// <summary>
@@ -10,44 +12,6 @@ namespace EksamensProjekt2023.Models;
 /// 
 /// A simple user model.
 /// </summary>
-public class User
+public class User : IdentityUser
 {
-    public User() 
-    {
-        UserID = Guid.NewGuid().ToString();
-        FirstName = string.Empty;
-        LastName = string.Empty;
-        Email = string.Empty;
-        Username = string.Empty;
-        UserPassword = string.Empty;
-        IsAdmin = false;
-        IsLeader = false;
-        IsApproved = false;
-    }
-
-    public User(string firstName, string lastName, string email, int phoneNumber, string username, string userPassword)
-    {
-        UserID = Guid.NewGuid().ToString();
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        PhoneNumber = phoneNumber;
-        Username = username;
-        UserPassword = userPassword;
-        IsAdmin = false;
-        IsLeader = false;
-        IsApproved = false;
-    }
-
-    [Key]
-    public string UserID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public int PhoneNumber { get; set; }
-    public string Username { get; set; }
-    public string UserPassword { get; set; }
-    public bool IsAdmin { get; set; }
-    public bool IsLeader { get; set; }
-    public bool IsApproved { get; set; }
 }
