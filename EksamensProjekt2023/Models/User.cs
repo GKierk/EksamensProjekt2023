7cb1c0dbd8a420456eb518b8f2eb620b93340868
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-20 12:39:36 +0100

    Updated User class

--- EksamensProjekt2023/Models/User.cs
+++ EksamensProjekt2023/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace EksamensProjekt2023.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EksamensProjekt2023.Models;
 /// <summary>
 /// Date Created: 2023-11-14 <br />
 /// Author: Muhammed Tastan <br />
@@ -10,19 +12,42 @@
 /// </summary>
 public class User
 {
-    public int UserID { get; set; }
-    public string? Username { get; set; }
-    public string? UserPassword { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    public User() 
+    {
+        UserID = Guid.NewGuid().ToString();
+        FirstName = string.Empty;
+        LastName = string.Empty;
+        Email = string.Empty;
+        Username = string.Empty;
+        UserPassword = string.Empty;
+        IsAdmin = false;
+        IsLeader = false;
+        IsApproved = false;
+    }
+
+    public User(string firstName, string lastName, string email, int phoneNumber, string username, string userPassword)
+    {
+        UserID = Guid.NewGuid().ToString();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Username = username;
+        UserPassword = userPassword;
+        IsAdmin = false;
+        IsLeader = false;
+        IsApproved = false;
+    }
+
+    [Key]
+    public string UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
     public int PhoneNumber { get; set; }
+    public string Username { get; set; }
+    public string UserPassword { get; set; }
     public bool IsAdmin { get; set; }
     public bool IsLeader { get; set; }
     public bool IsApproved { get; set; }
-
-
-    public void SetAdmin() => IsAdmin = true;
-    public void SetLeader() => IsLeader = true;
-    public void SetApproved() => IsApproved = true;
 }
