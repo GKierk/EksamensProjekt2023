154674f68cae7185399cda854aad4dac3d319485
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-22 12:39:26 +0100

    Updated TastanDBContext to inherit from IdentityDBContext

--- EksamensProjekt2023/Models/TastanDBContext.cs
+++ EksamensProjekt2023/Models/TastanDBContext.cs
@@ -1,16 +1,17 @@
 ﻿using EksamensProjekt2023.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace EksamensProjekt2023.Models
 {
-    public class TastanDBContext : DbContext
+    public class TastanDBContext : IdentityDbContext<UserProfile>
     {
         public TastanDBContext(DbContextOptions<TastanDBContext> options) : base(options) 
         {
             
         }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
         //public DbSet<Group> Groups { get; set; }
 
     }
