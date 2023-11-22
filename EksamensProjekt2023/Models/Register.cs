383351dc7e669212612e12dedcdcfdec23e19924
GKierk <gabriel_kierkegaard@protonmail.com>
2023-11-22 12:40:04 +0100

    Implemented Register model

---
+++ EksamensProjekt2023/Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EksamensProjekt2023.Models;
+
+public class Register
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Passwords doesn't match")]
+    public string ConfirmPassword { get; set; }
+}
