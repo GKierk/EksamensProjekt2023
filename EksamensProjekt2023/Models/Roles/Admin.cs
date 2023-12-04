45dbd8bcdf1d163c18d2f51fcd82e5f1c76abf65
ThePurpleDevil <henrik.martin@hotmail.com>
2023-12-04 14:10:18 +0100

    Made Admin Subclass

---
+++ EksamensProjekt2023/Models/Roles/Admin.cs
@@ -0,0 +1,12 @@
+﻿namespace EksamensProjekt2023.Models.Roles
+/// <summary>
+/// Date Created: 2023-12-04 <br />
+/// Author: Henrik Martin Seindahl<br />
+
+{
+    public class Admin : Role
+    {
+ 
+
+    }
+}
