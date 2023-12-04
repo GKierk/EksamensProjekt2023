fcd265e4da48b485606f8662dff3e51b2361fc8b
ThePurpleDevil <henrik.martin@hotmail.com>
2023-12-04 14:11:23 +0100

    Made Member Subclass

---
+++ EksamensProjekt2023/Models/Roles/Member.cs
@@ -0,0 +1,12 @@
+﻿using EksamensProjekt2023.Models.Roles;
+
+namespace EksamensProjekt2023.Models
+/// <summary>
+/// Date Created: 2023-12-04 <br />
+/// Author: Henrik Martin Seindahl<br />
+{
+    public class Member : Role
+    {
+     
+    }
+}
