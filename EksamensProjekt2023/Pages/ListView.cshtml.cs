c34db4b755f6eed6a0090e9142af67db88ef8465
Fr0styy <lassehindsberg@gmail.com>
2023-12-06 15:24:56 +0100

    file added

---
+++ EksamensProjekt2023/Pages/ListView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamensProjekt2023.Pages
+{
+    public class ListViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
