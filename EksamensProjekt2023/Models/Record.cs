382aa5c5dd9ee067d037fb4288be6731d64ea3bc
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-11 11:57:43 +0100

    Implemented Record class

---
+++ EksamensProjekt2023/Models/Record.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EksamensProjekt2023.Models;
+
+/// <summary>
+/// Date Created: 2023-12-11 <br />
+/// Author: Gabriel H. Kierkegaard <br />
+/// <br />
+/// 
+/// </summary>
+public class Record
+{
+    public Record()
+    {
+        RecordID = Guid.NewGuid().ToString();
+        Date = DateTime.Now;
+        AmountGiven = 0;
+        AmountSold = 0;
+        MobilePayAmount = 0;
+    }
+
+    [Key]
+    public string RecordID { get; set; }
+    public DateTime Date { get; set; }
+    [ForeignKey("Id")]
+    public UserProfile SalesPerson { get; set; }
+    public int AmountGiven { get; set; }
+    public int AmountSold { get; set; }
+    public int MobilePayAmount { get; set; }
+}
