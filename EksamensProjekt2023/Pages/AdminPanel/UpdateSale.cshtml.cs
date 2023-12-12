0c6f896848a446dc7bbda24f27d1a604acb0dc4e
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-12 13:49:25 +0100

    Implemented UpdateSale

---
+++ EksamensProjekt2023/Pages/AdminPanel/UpdateSale.cshtml.cs
@@ -0,0 +1,44 @@
+using EksamensProjekt2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamensProjekt2023.Pages.AdminPanel;
+/// <summary>
+/// Date Created: 2023-12-12 <br />
+/// Author: Gabriel H. Kierkegaard <br />
+/// <br />
+/// 
+/// </summary>
+public class UpdateSaleModel : PageModel
+{
+    private TastanDBContext dBContext;
+
+    public UpdateSaleModel(TastanDBContext context)
+    {
+        dBContext = context;
+    }
+
+    [BindProperty]
+    public Record Record { get; set; }
+
+    public void OnGet(string RecordID)
+    {
+        Record = dBContext.Records.Find(RecordID);
+    }
+
+    public IActionResult OnPost(string RecordID)
+    {
+        Record updateRecord = dBContext.Records.Find(RecordID);
+
+        if (updateRecord != null)
+        {
+            updateRecord.AmountSold = Record.AmountSold;
+            updateRecord.MobilePayAmount = Record.MobilePayAmount;
+            dBContext.SaveChanges();
+            
+            return RedirectToPage("/AdminPanel/ReadSale");
+        }
+
+        return Page();
+    }
+}
