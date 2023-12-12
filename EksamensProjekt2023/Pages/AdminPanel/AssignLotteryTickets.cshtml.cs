2f0ae6fe20ff3f4128465d1b452f0843261a69ed
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-12 14:03:18 +0100

    Implemented AssignLotteryTickets

---
+++ EksamensProjekt2023/Pages/AdminPanel/AssignLotteryTickets.cshtml.cs
@@ -0,0 +1,44 @@
+using EksamensProjekt2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EksamensProjekt2023.Pages.AdminPanel;
+/// <summary>
+/// Date Created: 2023-12-12 <br />
+/// Author: Gabriel H. Kierkegaard <br />
+/// <br />
+/// 
+/// </summary>
+public class AssignLotteryTicketsModel : PageModel
+{
+    private TastanDBContext dBContext;
+
+    public AssignLotteryTicketsModel(TastanDBContext context)
+    {
+        dBContext = context;
+    }
+
+    [BindProperty]
+    public Record Record { get; set; }
+
+    public void OnGet(string RecordID)
+    {
+        Record = dBContext.Records.Find(RecordID);
+    }
+
+    public IActionResult OnPost(string RecordID)
+    {
+        Record updateRecord = dBContext.Records.Find(RecordID);
+
+        if (updateRecord != null)
+        {
+            updateRecord.AmountGiven = Record.AmountGiven;
+            dBContext.SaveChanges();
+
+            return RedirectToPage("/AdminPanel/ReadSale");
+        }
+
+        return Page();
+    }
+}
