c4c721c0eb7e7d8eb462d4c03a6b35bd3067f0cb
Muhammed Tastan <muha082195@gmail.com>
2023-12-14 00:43:47 +0100

    Update Group (ikke testet) uden Razor syntax i cshtml

---
+++ EksamensProjekt2023/Pages/Groups/UpdateGroup.cshtml.cs
@@ -0,0 +1,42 @@
+using EksamensProjekt2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EksamensProjekt2023.Pages.Groups
+/// <summary>
+/// Date Created: 2023-12-14 <br />
+/// Author: Muhammed Tastan <br />
+/// <br />
+/// 
+/// </summary>
+{
+    public class UpdateGroupModel : PageModel
+    {
+        private TastanDBContext dBContext;
+
+        public UpdateGroupModel(TastanDBContext context)
+        {
+            dBContext = context;
+        }
+
+        [BindProperty]
+        public Group Group { get; set; }
+
+        public void OnGet(string GroupID)
+        {
+            Group = dBContext.Groups.Include(g => g.Members).FirstOrDefault(g => g.GroupID == GroupID);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                dBContext.SaveChanges();
+                return RedirectToPage("/AdminPanel/ReadGroup");
+            }
+
+            return Page();
+        }
+    }
+}
