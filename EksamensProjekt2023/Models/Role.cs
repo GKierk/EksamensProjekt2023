37b4a521b5b8c05b46a3b89d6bfd7b4497dbb7a0
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-14 13:39:50 +0100

    Role inherits from IdentityRole

---
+++ EksamensProjekt2023/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EksamensProjekt2023.Models;
+/// <summary>
+/// Date Created: 2023-12-14 <br />
+/// Author: Gabriel H. Kierkegaard <br />
+/// <br />
+/// 
+/// </summary>
+public class Role : IdentityRole
+{
+
+}
