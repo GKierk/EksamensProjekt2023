ac5b0b180ba4624c2dd5d61e34c87e6d6c5406cc
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-14 06:28:45 +0100

    Modified

---
+++ EksamensProjekt2023/Pages/AdminPanel/DeleteSale.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EksamensProjekt2023.Pages.AdminPanel
+{
+    public class DeleteSaleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
