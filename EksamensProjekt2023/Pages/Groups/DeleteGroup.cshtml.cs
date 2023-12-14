a5796d5121ee5d263038e4147027bb23e8500879
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-14 14:02:32 +0100

    Commented out

--- EksamensProjekt2023/Pages/Groups/DeleteGroup.cshtml.cs
+++ EksamensProjekt2023/Pages/Groups/DeleteGroup.cshtml.cs
@@ -1,43 +1,43 @@
-using EksamensProjekt2023.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+//using EksamensProjekt2023.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
 
-namespace EksamensProjekt2023.Pages.Groups
-/// <summary>
-/// Date Created: 2023-12-14 <br />
-/// Author: Muhammed Tastan <br />
-/// <br />
-/// 
-/// </summary>
-{
-    public class DeleteGroupModel : PageModel
-    {
-        private TastanDBContext dBContext;
+//namespace EksamensProjekt2023.Pages.Groups
+///// <summary>
+///// Date Created: 2023-12-14 <br />
+///// Author: Muhammed Tastan <br />
+///// <br />
+///// 
+///// </summary>
+//{
+//    public class DeleteGroupModel : PageModel
+//    {
+//        private TastanDBContext dBContext;
 
-        public DeleteGroupModel(TastanDBContext context)
-        {
-            dBContext = context;
-        }
+//        public DeleteGroupModel(TastanDBContext context)
+//        {
+//            dBContext = context;
+//        }
 
-        [BindProperty]
-        public Group Group { get; set; }
+//        [BindProperty]
+//        public Group Group { get; set; }
 
-        public void OnGet(string GroupID)
-        {
-            Group = dBContext.Groups.Include(g => g.Members).FirstOrDefault(g => g.Id == GroupID);
-        }
+//        public void OnGet(string GroupID)
+//        {
+//            Group = dBContext.Groups.Include(g => g.Members).FirstOrDefault(g => g.Id == GroupID);
+//        }
 
-        public IActionResult OnPost()
-        {
-            if (Group != null)
-            {
-                dBContext.Groups.Remove(Group);
-                dBContext.SaveChanges();
-                return RedirectToPage("/AdminPanel/ReadGroup");
-            }
+//        public IActionResult OnPost()
+//        {
+//            if (Group != null)
+//            {
+//                dBContext.Groups.Remove(Group);
+//                dBContext.SaveChanges();
+//                return RedirectToPage("/AdminPanel/ReadGroup");
+//            }
 
-            return Page();
-        }
-    }
-}
+//            return Page();
+//        }
+//    }
+//}
--- EksamensProjekt2023/Pages/Groups/ReadGroup.cshtml.cs
+++ EksamensProjekt2023/Pages/Groups/ReadGroup.cshtml.cs
@@ -1,31 +1,31 @@
-using EksamensProjekt2023.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+//using EksamensProjekt2023.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
 
-namespace EksamensProjekt2023.Pages.Groups
-/// <summary>
-/// Date Created: 2023-12-14 <br />
-/// Author: Muhammed Tastan <br />
-/// <br />
-/// 
-/// </summary>
-{
-    public class ReadGroupModel : PageModel
-    {
-        private TastanDBContext dBContext;
+//namespace EksamensProjekt2023.Pages.Groups
+///// <summary>
+///// Date Created: 2023-12-14 <br />
+///// Author: Muhammed Tastan <br />
+///// <br />
+///// 
+///// </summary>
+//{
+//    public class ReadGroupModel : PageModel
+//    {
+//        private TastanDBContext dBContext;
 
-        public ReadGroupModel(TastanDBContext context)
-        {
-            dBContext = context;
-        }
+//        public ReadGroupModel(TastanDBContext context)
+//        {
+//            dBContext = context;
+//        }
 
-        [BindProperty]
-        public List<Group> Groups { get; set; } = new List<Group>();
+//        [BindProperty]
+//        public List<Group> Groups { get; set; } = new List<Group>();
 
-        public void OnGet()
-        {
-            Groups = dBContext.Groups.Include(g => g.Members).ToList();
-        }
-    }
-}
+//        public void OnGet()
+//        {
+//            Groups = dBContext.Groups.Include(g => g.Members).ToList();
+//        }
+//    }
+//}
