150b4a42f236fee391e61defb40eb3a835304224
Pepeg <Pepeg@LAPTOP-K4NPV0KM>
2023-12-14 14:04:39 +0100

    Commented out

--- EksamensProjekt2023/Pages/Groups/UpdateGroup.cshtml.cs
+++ EksamensProjekt2023/Pages/Groups/UpdateGroup.cshtml.cs
@@ -3,40 +3,40 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace EksamensProjekt2023.Pages.Groups
+//namespace EksamensProjekt2023.Pages.Groups
 /// <summary>
 /// Date Created: 2023-12-14 <br />
-/// Author: Muhammed Tastan <br />
+/// Author: Muhammed Tastan<br />
 /// <br />
 /// 
 /// </summary>
-{
-    public class UpdateGroupModel : PageModel
-    {
-        private TastanDBContext dBContext;
+//{
+//    public class UpdateGroupModel : PageModel
+//{
+//    private TastanDBContext dBContext;
 
-        public UpdateGroupModel(TastanDBContext context)
-        {
-            dBContext = context;
-        }
+//    public UpdateGroupModel(TastanDBContext context)
+//    {
+//        dBContext = context;
+//    }
 
-        [BindProperty]
-        public Group Group { get; set; }
+//    [BindProperty]
+//    public Group Group { get; set; }
 
-        public void OnGet(string GroupID)
-        {
-            Group = dBContext.Groups.Include(g => g.Members).FirstOrDefault(g => g.GroupID == GroupID);
-        }
+//    public void OnGet(string GroupID)
+//    {
+//        Group = dBContext.Groups.Include(g => g.Members).FirstOrDefault(g => g.GroupID == GroupID);
+//    }
 
-        public IActionResult OnPost()
-        {
-            if (ModelState.IsValid)
-            {
-                dBContext.SaveChanges();
-                return RedirectToPage("/AdminPanel/ReadGroup");
-            }
+//    public IActionResult OnPost()
+//    {
+//        if (ModelState.IsValid)
+//        {
+//            dBContext.SaveChanges();
+//            return RedirectToPage("/AdminPanel/ReadGroup");
+//        }
 
-            return Page();
-        }
-    }
-}
+//        return Page();
+//    }
+//}
+//}
