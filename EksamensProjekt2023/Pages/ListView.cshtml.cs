c00da83d3b069795c1d015fd3e398e9f84af9548
Fr0styy <lassehindsberg@gmail.com>
2023-12-14 07:16:44 +0100

    Added Group & Leader + list, prepared foreach loops for page data.

--- EksamensProjekt2023/Pages/ListView.cshtml.cs
+++ EksamensProjekt2023/Pages/ListView.cshtml.cs
@@ -1,14 +1,54 @@
+using EksamensProjekt2023.Models;
+using EksamensProjekt2023.Models.Roles;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EksamensProjekt2023.Pages
-{
+{/// <summary>
+/// Date Created: 2023-12-14 <br />
+/// Author: Lasse B. Hindsberg <br />
+/// <br />
+/// 
+/// </summary>
     public class ListViewModel : PageModel
     {
+        private TastanDBContext dBContext;
+
+        public ListViewModel(TastanDBContext context)
+        {
+            dBContext = context;
+        }
+        // Adds the string pageName to detect url later on
+        [BindProperty]
         public string pageName { get; set; }
+        // defines group
+        [BindProperty]
+        public Group Group { get; set; }
+        [BindProperty]
+        // makes group into a list to be used in forEach
+        public List<Group> Groups { get; set; } = new List<Group>();
+        [BindProperty]
+        // repeated with Leader
+        public Leader Leader { get; set; }
+        public List<Leader> Leaders { get; set; } = new List<Leader>();
         public void OnGet()
         {
+            // detects page from the URL parameters
             pageName = Request.Query["Page"];
+
+            // Loops over data in database to be printed in the tables on the page
+
+            /// REMEMBER TO REMOVE THE COMMENT FROM THIS PART
+/*            foreach (var group in dBContext.Groups)
+            {
+                Groups.Add(group);
+            }
+            // repeat with Leader
+            foreach (var leader in dBContext.Leaders)
+            {
+                Leaders.Add(leader);
+            }
+*/
         }
     }
 }
