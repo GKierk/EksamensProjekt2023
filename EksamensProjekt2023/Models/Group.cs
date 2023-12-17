c58d9fb3319282d7ed2bfa357fb3f0a105b3201d
Gabriel H. Kierkegaard <gabkierk@proton.me>
2023-12-17 14:24:23 +0100

    Missing ;

--- EksamensProjekt2023/Models/Group.cs
+++ EksamensProjekt2023/Models/Group.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EksamensProjekt2023.Models
+namespace EksamensProjekt2023.Models;
+
+/// <summary>
+/// Date Created: 2023-11-14 <br />
+/// Author: Gabriel H. Kierkegaard <br />
+/// <br />
+/// A Model of a group which contains its members and assigned leader.
+/// </summary>
+public class Group
 {
-    /// <summary>
-    /// Date Created: 2023-11-14 <br />
-    /// Author: Gabriel H. Kierkegaard <br />
-    /// <br />
-    /// A Model of a group which contains its members and assigned leader.
-    /// </summary>
-    public class Group
+    public Group()
     {
-        public Group()
-        {
-            Id = Guid.NewGuid().ToString();
-            GroupName = string.Empty;
-        }
-
-        public Group(string groupName, UserProfile groupLeader)
-        {
-            Id = Guid.NewGuid().ToString();
-            GroupName = groupName;
-            GroupLeader = groupLeader;
-        }
-
-        [Key]
-        public string Id { get; set; }
-
-        public string GroupName { get; set; }
-
-        [ForeignKey("GroupLeaderId")]
-        public virtual UserProfile GroupLeader { get; set; }
+        Id = Guid.NewGuid().ToString();
+        GroupName = string.Empty;
     }
+
+    public Group(string groupName, UserProfile groupLeader)
+    {
+        Id = Guid.NewGuid().ToString();
+        GroupName = groupName;
+        GroupLeader = groupLeader;
+    }
+
+    [Key]
+    public string Id { get; set; }
+
+    public string GroupName { get; set; }
+
+    [ForeignKey("GroupLeaderId")]
+    public virtual UserProfile GroupLeader { get; set; }
 }
