6cb757af209e884fb3404f1b23e3cc3207a8fe4e
Gabriel H. Kierkegaard <gabkierk@proton.me>
2023-12-17 14:24:08 +0100

    Fixed a bug that cause a error when registering

--- EksamensProjekt2023/Models/Group.cs
+++ EksamensProjekt2023/Models/Group.cs
@@ -1,35 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EksamensProjekt2023.Models;
-/// <summary>
-/// Date Created: 2023-11-14 <br />
-/// Author: Gabriel H. Kierkegaard <br />
-/// <br />
-/// 
-/// A Model of a group which contains it's members and assigned leader.
-/// </summary>
-public class Group
+namespace EksamensProjekt2023.Models
 {
-    public Group()
+    /// <summary>
+    /// Date Created: 2023-11-14 <br />
+    /// Author: Gabriel H. Kierkegaard <br />
+    /// <br />
+    /// A Model of a group which contains its members and assigned leader.
+    /// </summary>
+    public class Group
     {
-        Id = Guid.NewGuid().ToString();
-        Members = new List<UserProfile>();
-        GroupName = string.Empty;
-    }
+        public Group()
+        {
+            Id = Guid.NewGuid().ToString();
+            GroupName = string.Empty;
+        }
 
-    public Group(string groupName, UserProfile groupLeader)
-    {
-        Id = Guid.NewGuid().ToString();
-        Members = new List<UserProfile>();
-        GroupName = groupName;
-        GroupLeader = groupLeader;
-    }
+        public Group(string groupName, UserProfile groupLeader)
+        {
+            Id = Guid.NewGuid().ToString();
+            GroupName = groupName;
+            GroupLeader = groupLeader;
+        }
 
-    [Key]
-    public string Id { get; set; }
-    public string GroupName { get; set; }
-    public List<UserProfile> Members { get; set; }
-    [ForeignKey("Id")]
-    public virtual UserProfile? GroupLeader { get; set; }
+        [Key]
+        public string Id { get; set; }
+
+        public string GroupName { get; set; }
+
+        [ForeignKey("GroupLeaderId")]
+        public virtual UserProfile GroupLeader { get; set; }
+    }
 }
