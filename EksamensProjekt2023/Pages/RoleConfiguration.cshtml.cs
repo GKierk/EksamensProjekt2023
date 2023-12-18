bef2ee29facbfab43da41aa314eed7c990d15c9b
Fr0styy <lassehindsberg@gmail.com>
2023-12-18 09:53:56 +0100

    Import UserProfiles to page

--- EksamensProjekt2023/Pages/RoleConfiguration.cshtml.cs
+++ EksamensProjekt2023/Pages/RoleConfiguration.cshtml.cs
@@ -1,12 +1,35 @@
+using EksamensProjekt2023.Models;
+using EksamensProjekt2023.Pages.AdminPanel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EksamensProjekt2023.Pages
 {
     public class RoleConfigurationModel : PageModel
     {
+        private TastanDBContext dbContext;
+
+        public RoleConfigurationModel(TastanDBContext context)
+        {
+            dbContext = context;
+        }
+        [BindProperty]
+        public UserProfile UserProfile { get; set; }
+        [BindProperty]
+        public List<UserProfile> UserProfiles { get; set; } = new List<UserProfile>();
+
+        /* Needs to be able to:
+         * Import roles from Database
+         * Based on the input fields on the page, assign a new role to the user
+         * 
+         */
         public void OnGet()
         {
+            foreach (var user in dbContext.UserProfiles)
+            {
+                UserProfiles.Add(user);
+            }
         }
     }
 }
