d7c05a352d20ba1cd643cf9b12b8013fe1000b86
Fr0styy <lassehindsberg@gmail.com>
2023-12-18 10:11:04 +0100

    added functionality to get group from database

--- EksamensProjekt2023/Pages/AdminPanel.cshtml.cs
+++ EksamensProjekt2023/Pages/AdminPanel.cshtml.cs
@@ -1,3 +1,4 @@
+using EksamensProjekt2023.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace EksamensProjekt2023.Pages
 {
     public class AdminPanelModel : PageModel
     {
+        private TastanDBContext dbContext;
+
+        public AdminPanelModel(TastanDBContext context)
+        {
+            dbContext = context;
+        }
+        [BindProperty]
+        public Group group { get; set; }
+        [BindProperty]
+        public List<Group> groups { get; set; } = new List<Group>();
+
         public void OnGet()
         {
+            foreach (var group in dbContext.Groups) {
+            groups.Add(group);
+            }
         }
     }
 }
