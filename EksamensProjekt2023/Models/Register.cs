3eaa360cbe8f29302d5c4a81723556529a9b89ff
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-18 10:17:21 +0100

    Added FirstName and LastName to Register

--- EksamensProjekt2023/Models/Register.cs
+++ EksamensProjekt2023/Models/Register.cs
@@ -11,6 +11,12 @@ namespace EksamensProjekt2023.Models;
 public class Register
 {
     [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
 
