d453db954920246a12ebc3c3cdd8c25bc023c3d6
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-18 10:06:44 +0100

    Implemented GroupMember

---
+++ EksamensProjekt2023/Models/GroupMember.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EksamensProjekt2023.Models;
+
+[PrimaryKey(nameof(FullName), nameof(GroupName))]
+public class GroupMember
+{
+    [ForeignKey("FullName")]
+    public string FullName { get; set; }
+
+    [ForeignKey("GroupName")]
+    public string GroupName { get; set; }
+}
