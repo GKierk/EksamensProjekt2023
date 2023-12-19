ab8ccbf5836ba24fed2feba5411d6090605dcab2
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-19 23:17:13 +0100

    Database Migration

---
+++ EksamensProjekt2023/Migrations/20231219212209_GroupMemberUpdate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EksamensProjekt2023.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupMemberUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GroupName",
+                table: "GroupMembers",
+                newName: "GroupId");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "GroupMembers",
+                newName: "UserProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMembers_GroupId",
+                table: "GroupMembers",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMembers_AspNetUsers_UserProfileId",
+                table: "GroupMembers",
+                column: "UserProfileId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMembers_Groups_GroupId",
+                table: "GroupMembers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMembers_AspNetUsers_UserProfileId",
+                table: "GroupMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMembers_Groups_GroupId",
+                table: "GroupMembers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GroupMembers_GroupId",
+                table: "GroupMembers");
+
+            migrationBuilder.RenameColumn(
+                name: "GroupId",
+                table: "GroupMembers",
+                newName: "GroupName");
+
+            migrationBuilder.RenameColumn(
+                name: "UserProfileId",
+                table: "GroupMembers",
+                newName: "FullName");
+        }
+    }
+}
---
+++ EksamensProjekt2023/Migrations/20231219212819_MinorAdjustment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EksamensProjekt2023.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorAdjustment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
