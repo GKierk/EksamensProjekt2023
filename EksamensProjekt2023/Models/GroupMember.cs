ae5f4c6b6649fd325835719bee33d4c66aae500e
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-19 23:17:54 +0100

    Updated GroupMember

--- EksamensProjekt2023/Models/GroupMember.cs
+++ EksamensProjekt2023/Models/GroupMember.cs
@@ -4,12 +4,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EksamensProjekt2023.Models;
 
-[PrimaryKey(nameof(FullName), nameof(GroupName))]
+[PrimaryKey(nameof(UserProfileId), nameof(GroupId))]
 public class GroupMember
 {
-    [ForeignKey("FullName")]
-    public string FullName { get; set; }
+    public GroupMember() {}
 
-    [ForeignKey("GroupName")]
-    public string GroupName { get; set; }
+    public GroupMember(string groupId, UserProfile userProfile)
+    {
+        GroupId = groupId;
+        UserProfile = userProfile;
+    }
+
+    [ForeignKey("GroupId")]
+    public string GroupId { get; set; }
+
+    public virtual Group Group { get; set; }
+
+    [ForeignKey("UserProfileId")]
+    public string UserProfileId { get; set; }
+
+    public virtual UserProfile UserProfile { get; set; }
 }
