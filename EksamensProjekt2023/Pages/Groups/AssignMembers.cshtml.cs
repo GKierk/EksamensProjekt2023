5ceca6b8e37c46f9bdfc31f552b29e87c85194bd
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-20 00:47:50 +0100

    WIP

--- EksamensProjekt2023/Pages/Groups/AssignMembers.cshtml.cs
+++ EksamensProjekt2023/Pages/Groups/AssignMembers.cshtml.cs
@@ -4,62 +4,63 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace EksamensProjekt2023.Pages.Groups
+namespace EksamensProjekt2023.Pages.Groups;
+
+public class AssignMembersModel : PageModel
 {
-    public class AssignMembersModel : PageModel
+    private TastanDBContext dbContext;
+
+    public AssignMembersModel(TastanDBContext dbContext)
     {
-        private TastanDBContext dbContext;
+        this.dbContext = dbContext;
+        UserProfiles = dbContext.Users;
+        AssignMembers = new List<AssignMember>();
 
-        public AssignMembersModel(TastanDBContext dbContext)
+        foreach (var user in UserProfiles)
         {
-            this.dbContext = dbContext;    
-            AssignMembers = new List<AssignMember>();
+            //AssignMember member = new AssignMember(user);
+            //AssignMembers.Add(member);
+
+            AssignMembers.Add(new AssignMember { UserProfile = user, IsMember = false });
         }
+    }
 
-        [BindProperty]
-        public Group Group { get; set; }
+    [BindProperty]
+    public Group Group { get; set; }
 
-        [BindProperty]
-        public List<UserProfile> UserProfiles { get; set; }
+    [BindProperty]
+    public IEnumerable<UserProfile> UserProfiles { get; set; }
 
-        [BindProperty]
-        public IdentityRole Role { get; set; }
+    [BindProperty]
+    public IdentityRole Role { get; set; }
 
-        [BindProperty]
-        public UserRoles UserRoles { get; set; }
+    [BindProperty]
+    public UserRoles UserRoles { get; set; }
 
-        [BindProperty]
-        public List<AssignMember> AssignMembers { get; set; }
+    [BindProperty]
+    public List<AssignMember> AssignMembers { get; set; }
 
-        [BindProperty]
-        public List<GroupMember> GroupMembers { get; set; }
+    [BindProperty]
+    public List<GroupMember> GroupMembers { get; set; }
 
-        public void OnGet(string groupId)
+    public void OnGet(string groupId)
+    {
+        Group = dbContext.Groups.Find(groupId);
+        Role = dbContext.Roles.FirstOrDefault(r => r.Name == "Leader");
+    }
+
+    public void OnPost(string groupId)
+    {
+        foreach (var member in AssignMembers)
         {
-            Group = dbContext.Groups.Find(groupId);
-            Role = dbContext.Roles.FirstOrDefault(r => r.Name == "Leader");
-            UserProfiles = dbContext.Users.ToList();
-
-            foreach (var user in UserProfiles)
+            if (member.IsMember)
             {
-                AssignMember member = new AssignMember(user);
-                AssignMembers.Add(member);
+                dbContext.GroupMembers.Add(new GroupMember(groupId, member.UserProfile));
             }
         }
 
-        public IActionResult OnPost()
-        {
-            foreach (var member in AssignMembers)
-            {
-                if (member.IsMember)
-                {
-                    dbContext.GroupMembers.Add(new GroupMember(Group.Id, member.UserProfile));
-                }
-            }
+        dbContext.SaveChanges();
 
-            dbContext.SaveChanges();
-
-            return RedirectToPage("/Groups/ReadGroup");
-        }
+        //return RedirectToPage("/Groups/ReadGroup");
     }
 }
