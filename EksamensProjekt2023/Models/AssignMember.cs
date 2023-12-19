fb4cbf74e3de87638b734f2a53048878a9647ed4
GKierk <gabriel_kierkegaard@protonmail.com>
2023-12-19 23:17:30 +0100

    Implemented AssignMember

---
+++ EksamensProjekt2023/Models/AssignMember.cs
@@ -0,0 +1,13 @@
+﻿namespace EksamensProjekt2023.Models;
+
+public class AssignMember
+{
+    public AssignMember(UserProfile userProfile)
+    {
+        UserProfile = userProfile;
+        IsMember = false;
+    }
+
+    public UserProfile UserProfile { get; set; }
+    public bool IsMember { get; set; }
+}
