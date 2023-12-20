559c8d46132576c6353a4719ec76b704b65ac61f
Fr0styy <lassehindsberg@gmail.com>
2023-12-20 04:43:42 +0100

    removed old JS code

--- EksamensProjekt2023/wwwroot/js/site.js
+++ EksamensProjekt2023/wwwroot/js/site.js
@@ -2,13 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-// basic slidey nav code probably not being used but now made :D
-// Author: Lasse Breum Hindsberg 12-07-2023
-document.addEventListener('DOMContentLoaded', function() {
-    let toggleBtn = document.querySelector(".toggleBtn");
-    const adminPanelNav = document.querySelector(".adminPanelSide");
-    
-    toggleBtn.addEventListener('click', function() {
-        adminPanelNav.classList.toggle('open');
-    })
-});
